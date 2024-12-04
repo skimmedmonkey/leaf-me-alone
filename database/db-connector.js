2050e62be85a805bfad00bdf99916e0d2ba77535
Benjamin PremiReiller <premireb@classwork.engr.oregonstate.edu>
2024-12-03 18:41:10 -0800

    reverted db-connector to kims7 account

--- database/db-connector.js
+++ database/db-connector.js
@@ -11,9 +11,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_premireb',
-    password        : '5414',
-    database        : 'cs340_premireb'
+    user            : 'cs340_kims7',
+    password        : '7343',
+    database        : 'cs340_kims7'
 })
 
 // Export it for use in our applicaiton
