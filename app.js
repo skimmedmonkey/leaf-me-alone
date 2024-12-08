891fef5ffddf2c512726515c1007bd102e8fdfb5
Steven Kim <kims7@oregonstate.edu>
2024-12-08 10:13:10 -0800

    revert port back to 5757

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 57575;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 5757;                 // Set a port number at the top so it's easy to change in the future
 app.use(express.static('public'));
 
 // Database
